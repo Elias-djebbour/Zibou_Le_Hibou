9ccafaa49a3b45e3842917a86a6567d4890a8fbf
Djebbour <djebbour2912@gmail.com>
2021-07-28 22:51:17 +0200

    Add empty project

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed; // vitesse de la patrol
+    public Transform[] waypoints; // les destinations
+
+    public Transform target; // la prochaine destinations
+    public int destPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On détermine la distance entre la target et l'ennemi
+        Vector3 direction = target.position - transform.position; 
+        // On défini le déplacement de la patrol
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World); 
+
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groudCheckLeft;
+    public Transform groudCheckRight;
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(groudCheckLeft.position, groudCheckRight.position);
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            isJumping = true; 
+        }
+
+        PlayerMove(horizontalMovement);     
+    }
+
+    void PlayerMove(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f);
+
+        if(isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+}
